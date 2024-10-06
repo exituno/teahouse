8f8e0a34178f4d5955a7ba8c752a3f4f23531534
Joey de Vries <13612647+vanvanni@users.noreply.github.com>
2024-10-06 19:37:42 +0200

    Update tests for docker.info

--- tests/docker_test.ts
+++ tests/docker_test.ts
@@ -1,6 +1,13 @@
-import { assertEquals } from "jsr:@std/assert";
+import { assert, assertEquals, assertExists } from "jsr:@std/assert";
+import Docker from "../docker-api/mod.ts";
 
-Deno.test("Test Sample", () => {
-    const x = 1 + 2;
-    assertEquals(x, 3);
+Deno.test("Docker /info", async () => {
+    const engine = new Docker("tcp://localhost:2375");
+    const info = await engine.info();
+
+    assertEquals(info.success, true);
+    assertExists(info.data)
+    assert(info.data.Containers >= 0);
+    assert(info.data.Images >= 0);
+    assert(typeof info.data.ServerVersion === "string");
 });
