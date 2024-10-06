3fcc828ad90d1470d53b595df60f4dcc2045ac3c
Joey de Vries <13612647+vanvanni@users.noreply.github.com>
2024-10-06 20:37:42 +0200

    Add trpc for the api

--- main.ts
+++ main.ts
@@ -1,6 +1,27 @@
 import Docker from "./docker-api/mod.ts";
 
+import nhttp, { RequestEvent } from "jsr:@nhttp/nhttp";
+import adapter from "@nhttp/trpc";
+import { initTRPC } from "npm:@trpc/server";
+
 if (import.meta.main) {
   const engine = new Docker("tcp://localhost:2375");
-  console.log(await engine.containers.list())
+
+  const t = initTRPC.context<RequestEvent>().create();
+  const router = t.router;
+
+  const appRouter = router({
+    ping: t.procedure.query(() => {
+      return { message: "pong" };
+    }),
+  });
+
+  const app = nhttp();
+  app.use("/", adapter({ router: appRouter }));
+
+  app.listen(8000, (_err, info) => {
+    console.log(`Running on port ${info.port}`);
+  });
+
+  console.log(await engine.volumes.list());
 }
